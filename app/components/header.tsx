adef32b5434c904a3fa0a04638915533117de605
BYUI-Student2081190 <cscott1@byupathway.edu>
2026-02-19 13:34:19 -0700

    Bug fixes and sign out button added

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,12 +1,14 @@
 'use client';
 
-import Navigation from "./navigation";
+interface HeaderProps {
+  children: React.ReactNode;
+}
 
-export default function Header() {
+export default function Header({ children }: HeaderProps) {
     // Return nav in the header
     return (
         <header className="relative w-full border-b bg-white py-12">
-            <Navigation />
+            { children }
         </header>
     );
 };
